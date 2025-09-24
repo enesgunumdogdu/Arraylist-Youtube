56068c9c6bb696ed0ded710aaf64113655f0811b
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-09-24 22:08:29 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        ArrayList<String> isimler = new ArrayList<>();
+
+        System.out.println("Sıfırıncı durum arraylist=" + isimler);
+
+        isimler.add("Enes");
+        isimler.add("Mustafa");
+        isimler.add("Kemal");
+
+        System.out.println("Birinci durum arraylist=" + isimler);
+
+        isimler.add(1,"Elif");
+        System.out.println("İkinci durum arraylist=" + isimler);
+
+        System.out.println("2.indexdeki eleman=" + isimler.get(2));
+
+        isimler.remove("Enes");
+        System.out.println("Üçüncü durum arraylist=" + isimler);
+
+        System.out.println("Listedeki eleman sayısı=" + isimler.size());
+
+        isimler.clear();
+        System.out.println("Dördüncü durum arraylist=" + isimler);
+
+
     }
 }
